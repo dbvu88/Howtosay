27a2a66d3866ef440a28103a03f21f249700ce32
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-14 06:30:12 +0700

    directory setup

---
+++ backend/Server/index.js
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+import cors from 'cors'
+import express from 'express'
+import bodyParser from 'body-parser'
+import routes from '../Routes/'
+
+
+
+console.log('Hello Node.js project.');
+
+console.log(process.env.MY_SECRET);
+
+const app = express()
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// custom middleware
+app.use((req, res, next) => (
+    next()
+))
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.use('/api/users', routes.user);
+
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log('listening on port ' + PORT)
+})
