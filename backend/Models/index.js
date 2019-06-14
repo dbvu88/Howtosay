73c82aa6f5e65f37c18390fa8967fb90793aef66
ducbavu531@gmail.com <ducbavu531@gmail.com>
2019-06-14 10:47:24 +0700

    added index.js for model

---
+++ backend/Models/index.js
@@ -0,0 +1,19 @@
+let users = {
+    1: {
+      id: '1',
+      username: 'Robin Wieruch',
+    },
+    2: {
+      id: '2',
+      username: 'Dave Davids',
+    },
+    3: {
+      id: '3',
+      username: 'Dave Davids',
+    },
+    4: {
+      id: '4',
+      username: 'Dave Davids',
+    },
+  };
+  
