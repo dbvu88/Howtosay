a3999fd773c4a0635a0791d0c76475aaece6a614
Duc Vu <de.yd315@gmail.com>
2019-06-14 10:46:57 +0700

    added user model

---
+++ backend/Models/user.js
@@ -0,0 +1,32 @@
+const user = (sequelize, DataTypes) => {
+    const User = sequelize.define('user', {
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+        }
+    })
+
+    // defines 1 to many relationship
+    User.associate = models => {
+        User.hasMany(models.Question)
+    }
+
+    // find user by login ingo
+    User.findByLogin = async login => {
+        let user = await User.findOne({
+            where: { username: login }
+        })
+
+        if (!user) {
+            user = await User.findOne({
+                where: { email: login }
+            })
+        }
+
+        return user;
+    }
+
+    return User
+}
+
+export default user
