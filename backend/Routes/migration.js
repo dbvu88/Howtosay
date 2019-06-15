6efe4b2b4f963f404bdd019a64b8ecbb4b8cd3e7
Duc Vu <de.yd315@gmail.com>
2019-06-15 05:21:18 -0500

    refactored codes

--- backend/Routes/migration.js
+++ backend/Routes/migration.js
@@ -2,28 +2,35 @@ import db from '../Database'
 import { Router } from 'express'
 const router = Router()
 
-const execute = (sql, message) => {
+const execute = (res, sql, message=null) => {
     db.query(sql, (err, result) => {
-        if(err) throw err
-        console.log(result)
-        res.send(message)
+        if(err) {
+            // console.log(err.sqlMessage)
+            res.status(500).json(err)
+        }
+        res.status(200).send({
+            message,
+            result
+        })
     })
 }
 
 // create db
 router.get('/createdb', (req, res) => {
     const sql = 'CREATE DATABASE Howtosay'
-    execute(sql, 'Database created ...')
+    execute(res, sql, 'Database created ...')
 })
 
 // create table
 router.get('/createuserstable', (req, res) => {
-    const sql = `CREATE TABLE users(id int AUTO_INCREMENT,
+    const sql = `CREATE TABLE users(
+        id int AUTO_INCREMENT PRIMARY KEY,
         username VARCHAR(255),
         password VARCHAR(255),
-        email VARCHAR(255))`
+        email VARCHAR(255)
+        )`
 
-    execute(sql, 'Users Tabel created ...')
+    execute(res, sql, 'Users Table created ...')
 })
 
 export default router
